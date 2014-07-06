be5ff86b917a328113e9136a0530797945d4824e
LeastAction <JacobsonMT@gmail.com>
2014-07-06 15:43:16 -0700

    meaningless commit

--- Test Project/src/test/HelloWorld.java
+++ Test Project/src/test/HelloWorld.java
@@ -3,6 +3,6 @@ package test;
 public class HelloWorld {
 	  public static void main(String[] args) {
 		    System.out.println("Hello World!");	  
-		    System.out.println("Git is fun");
+		    System.out.println("Git is really fun");
 		  }
 }
